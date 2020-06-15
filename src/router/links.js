d96388d54e2ea8b244555b5426eed6ace71213b2
Dylan Aird <dylan.aird595@gmail.com>
2020-06-15 18:11:08 +1000

    first commit

---
+++ src/router/links.js
@@ -0,0 +1,15 @@
+const links = [
+  {
+    title: 'Home',
+    caption: 'Dashboard',
+    icon: 'school',
+    link: ''
+  },
+  {
+    title: 'Import',
+    caption: 'Import',
+    icon: 'code',
+    link: '/export'
+  }
+]
+export default links
